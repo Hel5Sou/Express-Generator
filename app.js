4cce2f31618be59ba72bd5d06669e1c973f87031
Helena <helena.soukupova5@gmail.com>
2021-01-11 16:26:22 -0800

    Basic Authentication

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+
+
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
@@ -34,6 +36,32 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+function auth(req, res, next) {
+  console.log(req.headers);
+  const authHeader = req.headers.authorization;
+  if (!authHeader) { //for if it's null = without password
+    const err = new Error('You are not authenticated!');
+    res.setHeader('WWW-Authenticate', 'Basic'); // server required auth and it's Basic
+    err.status = 401;
+    return next(err);
+  }
+
+  const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':'); // challenging client for credential and here parse the auth header and authorize the username and password
+  const user = auth[0];
+  const pass = auth[1];
+  if (user === 'admin' && pass === 'password') {
+    return next(); // authorized
+  } else {
+    const err = new Error('You are not authenticated!');
+    res.setHeader('WWW-Authenticate', 'Basic');
+    err.status = 401;
+    return next(err);
+  }
+}
+
+app.use(auth);
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
