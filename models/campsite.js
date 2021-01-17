e02d6d008edc7a8bd5838f1fa36398f8bc7d47a7
Helena <helena.soukupova5@gmail.com>
2021-01-17 14:34:57 -0800

    Mongoose Population

--- models/campsite.js
+++ models/campsite.js
@@ -15,9 +15,9 @@ const commentSchema = new Schema({
         type: String,
         required: true
     },
-    author: {
-        type: String,
-        required: true
+    author: { //store reference to use document through the Object.Id
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
     }
 }, {
     timestamps: true
